f2f899e871eeabdd31ca567078767e78d79f3800
schachbulle <hoppe.berlin@googlemail.com>
2020-04-19 19:26:11 +0200

    Übernahme der Version aus Contao 3

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Berolina-GrandPrix Changelog
+
+## Version 0.0.1 (2020-04-19)
+
+- Übernahme der Version aus Contao 3
---
+++ src/Resources/contao/languages/de/tl_content.php
@@ -0,0 +1,21 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (C) 2005-2013 Leo Feyer
+ *
+ * @package   fen
+ * @author    Frank Hoppe
+ * @license   GNU/LGPL
+ * @copyright Frank Hoppe 2013
+ */
+
+$GLOBALS['TL_LANG']['tl_content']['berolina_grandprix_list']['0'] = "Meisterschaft";
+$GLOBALS['TL_LANG']['tl_content']['berolina_grandprix_list']['1'] = "Meisterschaft auswählen";
+$GLOBALS['TL_LANG']['tl_content']['berolina_grandprix_tourcount']['0'] = "Anzahl Turniere";
+$GLOBALS['TL_LANG']['tl_content']['berolina_grandprix_tourcount']['1'] = "Gesamtstand nach wieviel Turnieren anzeigen?";
+
+$GLOBALS['TL_LANG']['tl_content']['berolina_grandprix_legend'] = 'Einstellungen';
+
+?>
